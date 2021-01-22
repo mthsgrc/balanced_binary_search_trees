0d3ce50f31327dc1240b5ccb358096bc7618e73f
mthsgrc <matheus.dev.garcia@gmail.com>
2021-01-22 15:43:33 -0300

    level order method. Breath level returns array of values.

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -13,9 +13,9 @@ end
 class Tree
   attr_accessor :root, :array
 
-  def initialize(array = [])
+  def initialize(array)
     @array = sort_array(array)
-    p @array
+    # p @array
     @root = build_tree(array)
   end
 
@@ -39,48 +39,61 @@ class Tree
 
   def insert(value, root = @root)
     # binding.pry
+    #value already exists
     if value == root.data
       return nil
     end
 
+    #if value is greater than root, insert to right
     if root.data < value
       root.right.nil? ? root.right = Node.new(value) : insert(value, root.right)
+      #otherwise, that is, the value is less than root, insert left
     else
       root.left.nil? ? root.left = Node.new(value) : insert(value, root.left)
     end
   end
 
+  #find min value in tree
   def min_value(node)
-	current = node 
-
-	while(current.left != nil) 
-		current = current.left
-	end	
-	return current
+    current = node
+    while(current.left != nil)
+      current = current.left
+    end
+    return current.data
   end
 
+
   def delete(value, root = @root)
+    #base case
     return root if root.nil?
 
+    # Value is in left or right subtree comparing with root values
     if value < root.data
       root.left = delete(value, root.left)
     elsif value > root.data
       root.right = delete(value, root.right)
+    # Value is at root's key. This node is to be deleted
     else
+      # Node with only one child or no child	
       if root.left.nil?
         temp = root.right
         root = nil
         return temp
       elsif root.right.nil?
-      	temp = root.left
-      	root = nil
-      	return temp
+        temp = root.left
+        root = nil
+        return temp
       end
+
+      # Node with two children
+      # Get the min value in subtree
       temp = min_value(root.right)
 
-      root.data = temp.data
+      # Copy the successor content to this node
+      root.data = temp
 
-      root.right = delete(temp.data, root.right)
+      # Delete inoder successor
+      root.right = delete(root.data, root.right)
     end
     return root
   end
@@ -100,6 +113,32 @@ class Tree
     end
   end
 
+  def level_order(root = @root, queue = [], output = [])
+    return nil if root.nil?
+
+    queue << root
+
+    while !queue.empty?
+      current = queue.shift
+
+      # binding.pry
+      queue << current.left if current.left != nil
+      queue << current.right if current.right != nil
+
+      output << current.data
+    end
+    output
+
+    # below is the same recursively
+    # output << root.data
+    # queue << root.left unless root.left.nil?
+    # queue << root.right unless root.right.nil?
+    # p output
+    # return if queue.empty?
+    # level_order(queue.shift, queue, output)
+    # output
+  end
+
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
@@ -111,6 +150,16 @@ class Tree
 
 end
 
+# │                   ┌── 31
+# │               ┌── 11
+# │           ┌── 10
+# │       ┌── 7
+# │   ┌── 6
+# │   │   └── 5
+# └── 4
+#     │   ┌── 3
+#     └── 1
+
 
 # array = [1, 7, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 array = [1, 3, 4, 7, 5, 6, 7]
@@ -121,22 +170,8 @@ bst.insert(10)
 bst.insert(11)
 bst.insert(31)
 
-# binding.pry
-puts
-
-# puts bst.find(3)
-# puts bst.find(10)
-
-puts
-bst.pretty_print
-
-bst.delete(10)
-bst.delete(31)
-bst.delete(4)
-
 
 bst.pretty_print
-# bst.is_leaf?(7)
-
+bst.level_order
 
 # bst.pretty_print
